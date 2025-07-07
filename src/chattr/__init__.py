9ecaaeb6494e974a19aa066583e0c7ac0664d8c0
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-07-07 20:23:07 +0300

    fix config

--- src/chattr/__init__.py
+++ src/chattr/__init__.py
@@ -2,28 +2,47 @@ from datetime import datetime
 from os import getenv
 from pathlib import Path
 from loguru import logger
-
 from chattr.graph import Graph
 
-DEBUG: bool = getenv(key="DEBUG", default="True").lower() == "true"
-SERVER_NAME: str = getenv(key="GRADIO_SERVER_NAME", default="localhost")
-SERVER_PORT: int = int(getenv(key="GRADIO_SERVER_PORT", default="8080"))
+from dotenv import load_dotenv
+load_dotenv()
+SERVER_URL: str = getenv(key="SERVER_URL", default="localhost")
+SERVER_PORT: int = getenv(key="SERVER_PORT", default=7860)
 CURRENT_DATE: str = datetime.now().strftime(format="%Y-%m-%d_%H-%M-%S")
+MCP_VOICE_GENERATOR: str = getenv(key="MCP_VOICE_GENERATOR", default="http://localhost:8001/")
+MCP_VIDEO_GENERATOR: str = getenv(key="MCP_VIDEO_GENERATOR", default="http://localhost:8002/")
+VECTOR_DATABASE_NAME: str = getenv(key="VECTOR_DATABASE_NAME", default="chattr")
+GROQ_MODEL_NAME: str = getenv(key="GROQ_MODEL_NAME", default="llama-3.1-8b-instant")
+GROQ_MODEL_TEMPERATURE: float = getenv(key="GROQ_MODEL_TEMPERATURE", default=0.0)
 
 BASE_DIR: Path = Path.cwd()
-RESULTS_DIR: Path = BASE_DIR / "results"
+ASSETS_DIR: Path = BASE_DIR / "assets"
 LOG_DIR: Path = BASE_DIR / "logs"
-AUDIO_FILE_PATH: Path = RESULTS_DIR / f"{CURRENT_DATE}.wav"
-LOG_FILE_PATH: Path = LOG_DIR / f"{CURRENT_DATE}.log"
+IMAGE_DIR: Path = ASSETS_DIR / "image"
+AUDIO_DIR: Path = ASSETS_DIR / "audio"
+VIDEO_DIR: Path = ASSETS_DIR / "video"
 
-RESULTS_DIR.mkdir(exist_ok=True)
+LOG_FILE_PATH: Path = LOG_DIR / f"{CURRENT_DATE}.log"
+AUDIO_FILE_PATH: Path = AUDIO_DIR / f"{CURRENT_DATE}.wav"
+VIDEO_FILE_PATH: Path = VIDEO_DIR / f"{CURRENT_DATE}.mp4"
+
+ASSETS_DIR.mkdir(exist_ok=True)
+IMAGE_DIR.mkdir(exist_ok=True)
+AUDIO_DIR.mkdir(exist_ok=True)
+VIDEO_DIR.mkdir(exist_ok=True)
 LOG_DIR.mkdir(exist_ok=True)
 
 GRAPH = Graph()
 
+logger.add(
+    sink=LOG_FILE_PATH,
+    format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+    colorize=True,
+)
+
 logger.info(f"Current date: {CURRENT_DATE}")
 logger.info(f"Base directory: {BASE_DIR}")
-logger.info(f"Results directory: {RESULTS_DIR}")
+logger.info(f"Assets directory: {ASSETS_DIR}")
 logger.info(f"Log directory: {LOG_DIR}")
 logger.info(f"Audio file path: {AUDIO_FILE_PATH}")
 logger.info(f"Log file path: {LOG_FILE_PATH}")
