355e024d6095e80a49c730742fd39cf9de81565c
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-07-12 04:05:14 +0300

    Remove unused files and add new tools implementation

--- src/chattr/__main__.py
+++ src/chattr/__main__.py
@@ -1,46 +1,20 @@
-from chainlit import (
-    LangchainCallbackHandler,
-    Message,
-    context,
-    on_chat_start,
-    on_message,
-)
-from chainlit.cli import run_chainlit
-from langchain.schema.runnable.config import RunnableConfig
-from langchain_core.messages import HumanMessage
+from gradio import Blocks
 
-from chattr import logger
-from chattr.graph import Graph
+from chattr import SERVER_PORT, SERVER_URL
+from chattr.gui import app_block
 
 
 def main() -> None:
-    graph: Graph = Graph()
-
-    @on_chat_start
-    def on_start() -> None:
-        logger.info("A new chat session has started!")
-
-    @on_message
-    async def on_messaging(msg: Message):
-        config = {"configurable": {"thread_id": context.session.id}}
-        cb = LangchainCallbackHandler()
-        final_answer = Message(content="")
-
-        for stream_msg, metadata in graph.graph.stream(
-            {"messages": [HumanMessage(content=msg.content)]},
-            stream_mode="messages",
-            config=RunnableConfig(callbacks=[cb], **config),
-        ):
-            if (
-                stream_msg.content
-                and not isinstance(stream_msg, HumanMessage)
-                and metadata["langgraph_node"] == "final"
-            ):
-                await final_answer.stream_token(stream_msg.content)
-
-        await final_answer.send()
-
-    run_chainlit(__file__)
+    app: Blocks = app_block()
+    app.queue(api_open=True).launch(
+        server_name=SERVER_URL,
+        server_port=SERVER_PORT,
+        debug=True,
+        show_api=True,
+        enable_monitoring=True,
+        show_error=True,
+        pwa=True,
+    )
 
 
 if __name__ == "__main__":
