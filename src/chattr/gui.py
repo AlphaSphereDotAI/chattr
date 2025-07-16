1c5f116de9ac7988b6296f47b617eb0cae7e8ba2
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-07-16 03:15:18 +0300

    Refactor graph creation to support multiple MCP clients and tools, enabling vocalizr and visualizr functionalities.

--- src/chattr/gui.py
+++ src/chattr/gui.py
@@ -1,28 +1,43 @@
-import gradio
-from gradio import Blocks, Button, Chatbot, ChatMessage, Row, Textbox
+from gradio import (
+    Blocks,
+    ChatMessage,
+    Error,
+    LikeData,
+    TabbedInterface,
+    ChatInterface,
+    Request,
+)
 
 
-def generate_response(
-    history: list[ChatMessage], thread_id: str
-) -> list[ChatMessage]:
+def generate_response(text, thread_id: str):
     """
     Appends an assistant message about a quarterly sales plot to the chat history for the specified thread ID.
 
-    If the thread ID is 0, raises a Gradio error prompting for a valid thread ID.
+    If the thread ID is 0, it raises a Gradio error prompting for a valid thread ID.
 
     Returns:
         The updated chat history including the new assistant message.
     """
-    if thread_id == 0:
-        raise gradio.Error("Please enter a thread ID.")
-    history.append(
+    if thread_id == "0":
+        raise Error("Please enter a thread ID.")
+
+    yield [
         ChatMessage(
             role="assistant",
-            content=f"Here is the plot of quarterly sales for {thread_id}.",
+            content=f"{text} {thread_id}.",
             metadata={"title": "🛠️ Used tool Weather API"},
         )
-    )
-    return history
+    ]
+
+
+def like(evt: LikeData):
+    print("User liked the response")
+    print(evt.index, evt.liked, evt.value)
+
+
+def random_response(message, history, request: Request):
+    print(request.session_hash)
+    return f"{message} {history}"
 
 
 def app_block() -> Blocks:
@@ -33,29 +48,5 @@ def app_block() -> Blocks:
         Blocks: The complete Gradio Blocks interface for the chat application.
     """
 
-    history = [
-        ChatMessage(role="assistant", content="How can I help you?"),
-        ChatMessage(
-            role="user", content="Can you make me a plot of quarterly sales?"
-        ),
-        ChatMessage(
-            role="assistant",
-            content="I am happy to provide you that report and plot.",
-        ),
-    ]
-    with Blocks() as app:
-        with Row():
-            thread_id: Textbox = Textbox(
-                label="Thread ID", info="Enter Thread ID"
-            )
-
-        chatbot: Chatbot = Chatbot(history, type="messages")
-
-        with Row():
-            generate_btn: Button = Button(value="Generate", variant="primary")
-            stop_btn: Button = Button(value="Stop", variant="stop")
-        _event = generate_btn.click(
-            generate_response, [chatbot, thread_id], chatbot
-        )
-        stop_btn.click(cancels=[_event])
-    return app
+    chat = ChatInterface(random_response, type="messages", save_history=True)
+    return TabbedInterface([chat], ["Chattr"])
