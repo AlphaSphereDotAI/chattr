85df2dd6d039d45786ba7c24b5f177ab0f7ed185
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-07-30 06:01:37 +0300

    - Add `test_gradio.py` for testing Gradio client predictions. - Refactor `utils.py` to improve chunk handling in file downloads. - Update GUI to include `ClearButton` support for video. - Refine type hints and docstrings across several modules for improved clarity. - Update `compose.yaml` to use `REDIS_URL`. - Add `pytest` as a dependency and include related updates to `uv.lock` and `pyproject.toml`. - Adjust `.idea` configuration for data sources.

--- src/chattr/__main__.py
+++ src/chattr/__main__.py
@@ -4,9 +4,7 @@ from chattr.gui import app_block
 
 
 def main() -> None:
-    """
-    Initializes and launches the Gradio-based Chattr application server with API access, monitoring, and PWA support enabled.
-    """
+    """Initializes and launches the Gradio-based Chattr application server with API access, monitoring, and PWA support enabled."""
     app: Blocks = app_block()
     app.queue(api_open=True).launch(
         server_port=7860,
@@ -14,7 +12,7 @@ def main() -> None:
         show_api=True,
         enable_monitoring=True,
         show_error=True,
-        pwa=True,
+        pwa=True
     )
 
 
