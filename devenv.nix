a2ac0eb5890c0ee31df32217e6bdfa0f693056f2
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-10-07 01:28:10 +0300

    Enhance devenv.nix configuration with improved process management and environment variables

--- devenv.nix
+++ devenv.nix
@@ -1,15 +1,19 @@
-{
-  pkgs,
-  lib,
-  config,
-  inputs,
-  ...
+{ pkgs
+, lib
+, config
+, inputs
+, ...
 }:
 
 {
+  name = "chattr";
+  dotenv.enable = true;
   # https://devenv.sh/basics/
   env = {
     GREET = "devenv";
+    MODEL__URL = "https://generativelanguage.googleapis.com/v1beta/openai/";
+    # MODEL__API_KEY="${GEMINI_API_KEY}";
+    MODEL__NAME = "gemini-2.5-flash-lite";
   };
 
   # https://devenv.sh/packages/
@@ -28,25 +32,57 @@
 
   # https://devenv.sh/processes/
   # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+  processes = {
+    chattr = {
+      exec = "${lib.getExe pkgs.uv} run chattr";
+      process-compose = {
+        availability = {
+          backoff_seconds = 2;
+          max_restarts = 5;
+          restart = "on_failure";
+        };
+        depends_on = {
+          qdrant = {
+            condition = "process_completed_successfully";
+          };
+        };
+        # environment = [
+        #   "ENVVAR_FOR_THIS_PROCESS_ONLY=foobar"
+        # ];
+      };
+    };
+    qdrant.exec = "docker run --rm -d -p 6333:6333 -v 'qdrant_storage:/qdrant/storage:z' qdrant/qdrant";
+  };
 
   # https://devenv.sh/services/
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
+  # scripts = {
+  #   # hello.exec = ''
+  #   #   echo hello from $GREET
+  #   # '';
+  #   chattr.exec = "${lib.getExe pkgs.uv} run chattr";
+  # };
 
   # https://devenv.sh/basics/
   enterShell = ''
-    hello         # Run scripts directly
+    # hello         # Run scripts directly
     git --version # Use packages
   '';
 
   # https://devenv.sh/tasks/
   # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  #   "chattr:run" = {
+  #     exec = "${lib.getExe pkgs.uv} run chattr";
+  #     # execIfModified = [ "src" ];
+  #     after = [ "qdrant:start" ];
+  #   };
+  #   "qdrant:start" = {
+  #     exec = "docker run --rm -p 6333:6333 -v 'qdrant_storage:/qdrant/storage:z' qdrant/qdrant";
+  #     before = [ "devenv:processes:chattr" ];
+  #   };
+  #   # "devenv:enterShell".after = [ "myproj:setup" ];
   # };
 
   # https://devenv.sh/tests/
