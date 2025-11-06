63bd2ed0bce7130db126c95bbee9485faa1e495a
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-11-06 15:09:17 +0200

    Enhance main script output by using rich for formatted JSON display of Settings

--- src/chattr/app/settings.py
+++ src/chattr/app/settings.py
@@ -192,4 +192,6 @@ class Settings(BaseSettings):
 
 
 if __name__ == "__main__":
-    print(Settings().model_dump())
+    from rich import print as rprint
+
+    rprint(Settings().model_dump_json(indent=4))
