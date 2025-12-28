1a0cae15bc266dd7178860cd7881a9e4b0bdd5ff
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-12-28 23:17:16 +0200

    Refactor `__main__.py` to integrate async `Agent` setup and update app initialization

--- src/chattr/__main__.py
+++ src/chattr/__main__.py
@@ -1,22 +1,20 @@
-from typing import TYPE_CHECKING
+from anyio import run
 
-from chattr.app.runner import app
+from chattr.agent import Agent
+from chattr.app.app import App
+from chattr.app.settings import Settings
 
-if TYPE_CHECKING:
-    from gradio import Blocks
 
-
-def main() -> None:
-    """Launch the Gradio Multi-agent system app."""
-    application: Blocks = app.gui()
-    application.queue(api_open=True)
-    application.launch(
-        debug=True,
-        enable_monitoring=True,
-        show_error=True,
-        pwa=True,
-    )
+async def main() -> None:
+    """Launch the app."""
+    settings = Settings()
+    chattr_agent = Agent(settings)
+    agent = await chattr_agent.setup_agent()
+    application = App(agent)
+    gradio_application = application.gradio_app()
+    gradio_application.queue(api_open=True)
+    gradio_application.launch(debug=True, enable_monitoring=True, show_error=True, pwa=True)
 
 
 if __name__ == "__main__":
-    main()
+    run(main)
