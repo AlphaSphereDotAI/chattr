65fd5e838059212e6f066d8c86297424283f76a3
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-12-29 16:23:13 +0200

    Remove unused `runner.py` entry point from the application

--- src/chattr/app/runner.py
+++
@@ -1,6 +0,0 @@
-"""Entry point for initializing the application with settings."""
-from chattr.app.builder import App
-from chattr.app.settings import Settings
-
-settings: Settings = Settings()
-app: App = App(settings)
