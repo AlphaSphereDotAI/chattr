968a73aa454823d1aae3a6eb9393eb89f38f957e
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-12-30 20:45:42 +0200

    Simplify `__main__.py` by removing `setup_agent` and directly initializing `App` with `Settings`

--- src/chattr/__main__.py
+++ src/chattr/__main__.py
@@ -1,6 +1,5 @@
 from asyncio import run
 
-from chattr.agent.agent import setup_agent
 from chattr.app.app import App
 from chattr.app.settings import Settings
 
@@ -8,8 +7,7 @@ from chattr.app.settings import Settings
 async def main() -> None:
     """Launch the app."""
     settings = Settings()
-    agent = await setup_agent(settings)
-    application = App(agent, settings)
+    application = App(settings)
     gradio_application = application.gradio_app()
     gradio_application.queue(api_open=True)
     gradio_application.launch(debug=True, enable_monitoring=True, show_error=True, pwa=True)
