f4e1f9e7f8436729a91597a60dbb46c8973ee87e
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-01 17:34:37 +0200

    Refactor `setup_model` to use `ModelSettings` for improved parameter handling in `model.py`.

--- src/chattr/agent/model.py
+++ src/chattr/agent/model.py
@@ -1,40 +1,41 @@
 from agno.models.openai.like import OpenAILike
 
 from chattr.app.logger import logger
-from chattr.app.settings import Settings
+from chattr.app.settings import ModelSettings
 from chattr.app.utils import is_url
 
 
-def setup_model(settings: Settings) -> OpenAILike:
+def setup_model(model: ModelSettings) -> OpenAILike:
     """
-    Initialize the ChatOpenAI language model using the provided settings.
+    Initialize the OpenAILike language model using the provided settings.
 
-    This method creates and returns a ChatOpenAI instance configured with
+    This method creates and returns an OpenAILike instance configured with
     the model's URL, name, API key, and temperature.
 
     Returns:
-        ChatOpenAI: The initialized ChatOpenAI language model instance.
+        OpenAILike: The initialized OpenAILike language model instance.
     """
-    if not settings.model.url:
+    if not model.url:
         _msg = "Model URL is missing. Set it with `MODEL__URL`"
         logger.error(_msg)
         raise ValueError(_msg)
-    if not is_url(str(settings.model.url)):
+    if not is_url(model.url.encoded_string()):
         _msg = "Model URL is invalid. Set it with `MODEL__URL`"
         logger.error(_msg)
         raise ValueError(_msg)
-    if not settings.model.name:
+    if not model.name:
         _msg = "Model name is missing. Set it with `MODEL__NAME`"
         logger.error(_msg)
         raise ValueError(_msg)
-    if not settings.model.api_key:
+    if not model.api_key:
         _msg = "API key is missing. Set it with `MODEL__API_KEY`"
         logger.error(_msg)
         raise ValueError(_msg)
+    logger.info("Initializing OpenAILike language model")
     return OpenAILike(
-        base_url=str(settings.model.url),
-        id=settings.model.name,
-        api_key=settings.model.api_key.get_secret_value(),
-        temperature=settings.model.temperature,
+        base_url=model.url.encoded_string(),
+        id=model.name,
+        api_key=model.api_key.get_secret_value(),
+        temperature=model.temperature,
         cache_response=True,
     )
