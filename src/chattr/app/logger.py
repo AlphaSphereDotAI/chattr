efa9de35cb8837a21d6cc17cbbd21d916ca1e030
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-02 00:41:45 +0200

    Refactor logging setup to use `setup_logger` and `configure_agno_logging` for improved maintainability.

--- src/chattr/app/logger.py
+++ src/chattr/app/logger.py
@@ -1,21 +1,18 @@
 """A module handles the configuration of logging for the application."""
 
-from logging import INFO, WARNING, Logger, basicConfig, getLogger
+from logging import INFO, Formatter, Logger, getLogger
 
+from rich.console import Console
 from rich.logging import RichHandler
 
-from chattr import APP_NAME, console
 
-basicConfig(
-    level=INFO,
-    handlers=[
-        RichHandler(
-            level=INFO,
-            console=console,
-            rich_tracebacks=True,
-        ),
-    ],
-    format="%(name)s | %(process)d | %(message)s",
-)
-getLogger("httpx").setLevel(WARNING)
-logger: Logger = getLogger(APP_NAME)
+def setup_logger(logger_name: str | None) -> Logger:
+    logger: Logger = getLogger(logger_name)
+    console = Console()
+    handler = RichHandler(level=INFO, console=console, rich_tracebacks=True)
+    formatter = Formatter("%(name)s | %(process)d | %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(INFO)
+    logger.propagate = False
+    return logger
