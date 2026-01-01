83ee783567938071c3e711cf9cb25d2d70e6fce0
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-02 00:56:45 +0200

    Add type annotations to variables in `setup_logger` for better type safety

--- src/chattr/app/logger.py
+++ src/chattr/app/logger.py
@@ -8,9 +8,9 @@ from rich.logging import RichHandler
 
 def setup_logger(logger_name: str | None) -> Logger:
     logger: Logger = getLogger(logger_name)
-    console = Console()
-    handler = RichHandler(level=INFO, console=console, rich_tracebacks=True)
-    formatter = Formatter("%(name)s | %(process)d | %(message)s")
+    console: Console = Console()
+    handler: RichHandler = RichHandler(level=INFO, console=console, rich_tracebacks=True)
+    formatter: Formatter = Formatter("%(name)s | %(process)d | %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(INFO)
