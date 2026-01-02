7d2ff4c415bf3a6973dfa274fb8027b982be3edf
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-02 02:39:28 +0200

    Remove unused logging setup and configuration imports from `__init__.py`.

--- src/chattr/__init__.py
+++ src/chattr/__init__.py
@@ -1,12 +1,5 @@
 """A module for configuring and initializing."""
 
-from logging import Logger
 from warnings import filterwarnings
 
-from agno.utils.log import configure_agno_logging
-
-from chattr.app.logger import setup_logger
-
 filterwarnings("ignore", category=DeprecationWarning)
-logger: Logger = setup_logger(__package__)
-configure_agno_logging(custom_default_logger=logger)
