a6242a163588ab6536de3a03a70e902e10348831
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-09-21 19:11:31 +0300

    feat(mcp): Remove legacy MCP scheme and introduce new server configurations

--- src/chattr/settings/mcp.py
+++ src/chattr/settings/mcp.py
@@ -1,31 +1,34 @@
-from pathlib import Path
-from typing import Self
+from agno.tools.mcp import StreamableHTTPClientParams
+from pydantic import BaseModel, Field, HttpUrl
 
-from pydantic import BaseModel, Field, FilePath, model_validator
 
-from chattr.app.scheme import MCPScheme
+class VoiceGeneratorMCPServer(BaseModel, StreamableHTTPClientParams):
+    name: str = Field("voice_generator", description="Name of the MCP server instance")
+    url: HttpUrl = Field(
+        "http://localhost:7861/gradio_api/mcp",
+        description="URL of the Voice Generator MCP server instance",
+    )
+
+
+class VideoGeneratorMCPServer(BaseModel, StreamableHTTPClientParams):
+    name: str = Field("video_generator", description="Name of the MCP server instance")
+    url: HttpUrl = Field(
+        "http://localhost:7862/gradio_api/mcp/?tools=generate_video_mcp",
+        description="URL of the Video Generator MCP server instance",
+    )
+
+
+class ExtraMCPServer(BaseModel, StreamableHTTPClientParams):
+    name: str = Field("extra", description="Name of the MCP server instance")
+    url: HttpUrl = Field(
+        "http://localhost:7863/gradio_api/mcp/?tools=extra_mcp",
+        description="URL of the Extra MCP server instance",
+    )
 
 
 class MCPSettings(BaseModel):
     """Settings for MCP configuration."""
 
-    path: FilePath = Field(default_factory=lambda: Path.cwd() / "mcp.json")
-
-    @model_validator(mode="after")
-    def is_valid(self) -> Self:
-        """Validate that the MCP config file is a valid JSON file."""
-        if self.path:
-            if self.path.suffix != ".json":
-                msg = "MCP config file must be a JSON file"
-                raise ValueError(msg)
-            if self.path.stem != "mcp":
-                msg = "MCP config file must be named 'mcp.json'"
-                raise ValueError(msg)
-        return self
-
-    @model_validator(mode="after")
-    def is_valid_scheme(self) -> Self:
-        """Validate that the MCP config file has a valid scheme."""
-        if self.path and self.path.exists():
-            _ = MCPScheme.model_validate_json(self.path.read_text())
-        return self
+    mcp_servers: list[
+        VoiceGeneratorMCPServer | VideoGeneratorMCPServer | ExtraMCPServer
+    ] = Field(default_factory=list, description="List of MCP servers")
